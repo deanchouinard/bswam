e5e8160f83fe84facf408bd175636056f517bd20
Dean Chouinard <deanchouinard@gmail.com>
2015-04-18 10:44:38 -0400

    adding events

--- client/templates/examples.js
+++ client/templates/examples.js
@@ -5,6 +5,14 @@ Template.contextExample.rendered = function() {
 Template.contextExample.helpers({
 	logContext: function() {
 		console.log('Context Log helper', this);
+
+    return Session.get('randomNumber');
 	}
+});
+
+Template.contextExample.events({
+  'click button': function(){
+    Session.set('randomNumber', Math.random(0,99));
+  }
 });
 
