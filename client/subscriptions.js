c104a0725f2a6a8554208bbdeea49d16fc1dcfc1
Dean Chouinard <deanchouinard@gmail.com>
2015-05-14 07:54:38 -0400

    specific fields

---
+++ client/subscriptions.js
@@ -0,0 +1,1 @@
+Meteor.subscribe('specificfields-posts');
---
+++ server/publications.js
@@ -0,0 +1,17 @@
+Meteor.publish('all-posts', function () {
+  return Posts.find();
+});
+Meteor.publish('limited-posts', function () {
+  return Posts.find({}, {
+    limit: 2,
+    sort: {timeCreated: -1}
+  });
+});
+Meteor.publish('specificfields-posts', function () {
+  return Posts.find({}, {
+    fields: {
+      title: 1
+    }
+  });
+});
+
