ce7a3ec39879a2b4994e1d98f1a9043be9636890
Dean Chouinard <deanchouinard@gmail.com>
2015-05-15 22:58:33 -0400

    move posts sub to home route

--- client/subscriptions.js
+++
@@ -1,5 +0,0 @@
-Session.setDefault('lazyloadLimit', 2);
-Tracker.autorun(function(){
-Meteor.subscribe('lazyload-posts', Session.get('lazyloadLimit'));
-});
-
---
+++ routes.js
@@ -0,0 +1,23 @@
+if (Meteor.isClient) {
+  Session.setDefault('lazyloadLimit', 2);
+}
+
+Router.configure({
+  layoutTemplate: 'layout',
+  notFoundTemplate: 'notFound'
+});
+
+Router.map(function() {
+  this.route('Home', {
+    path: '/',
+    template: 'home',
+    subscriptions: function() {
+      return Meteor.subscribe('lazyload-posts', Session.get('lazyloadLimit'));
+    }
+  });
+  this.route('About', {
+    path: '/about',
+    template: 'about'
+  });
+});
+
