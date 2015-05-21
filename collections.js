d8f4fb2941067038a81e4c9a05a2eeb4c95badcf
Dean Chouinard <deanchouinard@gmail.com>
2015-05-20 22:33:35 -0400

    adding allow and deny rules

--- collections.js
+++ collections.js
@@ -1,2 +1,21 @@
 Posts = new Mongo.Collection('posts');
 
+
+if(Meteor.isServer) {
+  Posts.allow({
+    insert: function(userId, doc) {
+      return userId && doc.owner === userId && Meteor.user().roles.admin;
+    },
+    update: function(userID, doc, fields, modifier){
+      return Meteor.user().roles.admin;
+    },
+    fetch: ['owner']
+  });
+
+  Posts.deny( {
+    update: function(userId, docs, fields, modifier) {
+      return _.contains(fields, 'owner') || _.contains(fields, 'timeCreated')
+        || _.contains(fields, 'slug');
+    }
+  });
+}
